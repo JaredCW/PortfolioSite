8e33d002236e02a3167c3de7b3ea3f1cdb6e010c
Jared Conner <jared.w.conner@gmail.com>
2022-10-25 10:36:45 -0500

    First pass at notation and ToDo list

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// * I can't wait to do more with this!
+// TODO: Update default values to be more understandable. Don't forget the HTML!
+
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
